65bb9b3eb4e7ed9bcb36b8c222b3179ddc597bcd
MuhamadRyanHidayat <ryanhidayat074@gmail.com>
2019-11-16 13:46:05 +0700

    my frist web

---
+++ precache-manifest.e90a952c39c0a80f4dec9925b22adcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd927bbcefa144a64112534f4e651522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00ff91ba8b0494ea0edc",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1fbd5156bfa70a6d5d61",
+    "url": "/static/js/2.0e41ae2e.chunk.js"
+  },
+  {
+    "revision": "00ff91ba8b0494ea0edc",
+    "url": "/static/js/main.f4a72a26.chunk.js"
+  },
+  {
+    "revision": "772281873161fe35686a",
+    "url": "/static/js/runtime-main.875ef7b7.js"
+  },
+  {
+    "revision": "4cf0bed35f3f3be290b9be98a7ab7b3e",
+    "url": "/static/media/bg.4cf0bed3.png"
+  }
+]);
